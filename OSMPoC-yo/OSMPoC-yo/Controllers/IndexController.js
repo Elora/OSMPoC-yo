5892cc1b16da3059aa95dd19b9d1745e58790920
Kelly <samantha2286@gmail.com>
2015-10-31 14:10:38 +0100

    Start set-up menu and partial views

--- OSMPoC-yo/OSMPoC-yo/Controllers/IndexController.js
+++ OSMPoC-yo/OSMPoC-yo/Controllers/IndexController.js
@@ -1,6 +1,37 @@
-﻿var app = angular.module('OSMapp', []);
+﻿// create the module and name it
+var App = angular.module('OSMapp', ['ngRoute']);
 
-app.controller('MainController', function ($scope) {
-    $scope.test = "doei";
+// routes
+App.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'MapPartial.html',
+            controller: 'WorldMapController'
+        })
+        .when('/FriendList', {
+            templateUrl: 'FriendListPartial.html',
+            controller: 'FriendListController'
+        })
+        .when('/Achievements', {
+            templateUrl: 'AchievementsPartial.html',
+            controller: 'AchievementsOverviewController'
+        });
+});
 
+
+App.controller('IndexController', function ($scope) {
+    $scope.test = "Test";
+});
+
+// create the controller and inject Angular's $scope
+App.controller('WorldMapController', function ($scope) {
+    $scope.info = 'Wereld map';
+});
+
+App.controller('FriendListController', function ($scope) {
+    $scope.info = 'vriendjes';
+});
+
+App.controller('AchievementsOverviewController', function ($scope) {
+    $scope.info = 'Troep die je gehaald hebt';
 });
