729789344cec00c26c725819217aa6eca3b0a29b
Kelly <samantha2286@gmail.com>
2015-11-02 19:41:04 +0100

    Friendlist added

--- OSMPoC-yo/OSMPoC-yo/Controllers/FriendListController.js
+++ OSMPoC-yo/OSMPoC-yo/Controllers/FriendListController.js
@@ -1,3 +1,13 @@
 ﻿angular.module('OSMapp').controller('FriendListController', ['$scope', '$http', function ($scope, $http) {
     $scope.info = 'vriendjes';
+
+    //Henk his friends from the database
+    $scope.Friends = [
+            { name: 'Kelly', id: 1, mnpt: 200 },
+            { name: 'Rimmert', id: 2, mnpt: 650 },
+            { name: 'Freek', id: 3, mnpt: 20 },
+            { name: 'Rick', id: 4, mnpt: 25 },
+            { name: 'Samantha', id: 5, mnpt: 0 }
+    ];
+
 }]);
