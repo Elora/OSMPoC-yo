32ada686238962c04d2d56d3dbbcfcea5dba3eb9
Rimmert Koldenhof <groudon1111@gmail.com>
2015-11-02 14:22:52 +0100

    YO MAN FUCKING WORLD MAP FUCKING WORKING FUCKING CRAZY WINKLEVOSS TWINS SPIDERMAN MOVIE FUCK MAN

--- OSMPoC-yo/OSMPoC-yo/Controllers/WorldMapController.js
+++ OSMPoC-yo/OSMPoC-yo/Controllers/WorldMapController.js
@@ -1,4 +1,37 @@
 ﻿angular.module('OSMapp').controller('WorldMapController', ['$scope', '$http', function ($scope, $http) {
     $scope.info = 'Wereld map';
 
+    $scope.dummyData = [];
+
+}]);
+
+angular.module('OSMapp').directive('svgMap', ['$compile', function ($compile) {
+    return {
+        restrict: 'A',
+        templateUrl: '/Content/Worldmap-blank.svg',
+        link: function (scope, element, attrs) {
+            var regions = element[0].querySelectorAll('path');
+            angular.forEach(regions, function (path, key) {
+                var regionElement = angular.element(path);
+                regionElement.attr("region", "");
+                $compile(regionElement)(scope);
+            })
+        }
+    }
+}]);
+
+angular.module('OSMapp').directive('region', ['$compile', function ($compile) {
+    return {
+        restrict: 'A',
+        scope: true,
+        link: function (scope, element, attrs) {
+            scope.elementId = element.attr("id");
+            scope.regionClick = function () {
+                alert(scope.elementId);
+            };
+            element.attr("ng-click", "regionClick()");
+            element.removeAttr("region");
+            $compile(element)(scope);
+        }
+    }
 }]);
